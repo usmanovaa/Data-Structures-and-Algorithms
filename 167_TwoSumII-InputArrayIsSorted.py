e560a52e7dfc8797f246ace8f2d0f5c78f98bce5
usmanovaa <36270776+usmanovaa@users.noreply.github.com>
2023-02-01 10:21:42 +0300

    167. Two Sum II - Input Array Is Sorted

--- 167_TwoSumII-InputArrayIsSorted.py
+++ 167_TwoSumII-InputArrayIsSorted.py
@@ -1,11 +1,14 @@
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
 
-Your solution must use only constant extra space.
+# Your solution must use only constant extra space.
 
+# Binary Search, time complexity - O(logn)
+
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?envType=study-plan&id=algorithm-i
 
 class Solution(object):
     def twoSum(self, numbers, target):
@@ -15,12 +18,8 @@ class Solution(object):
         :rtype: List[int]
         """
 
-
         left = 0
         right = len(numbers) - 1
-
-        print(target)
-        print(numbers)
 
         while left < right:
             s = numbers[left] + numbers[right]
